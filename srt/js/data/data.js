69ebce59eb9979104bac89dc4f28adfc6e860b89
Jan Puig <janpuigcapella@gmail.com>
2021-01-15 12:56:57 +0100

    detectbotom when delete post

--- srt/js/data/data.js
+++ srt/js/data/data.js
@@ -1,82 +1,89 @@
-let start=0;
-let canLoadMore=true;
-let posts=[];
+let start = 0;
+let canLoadMore = true;
+let posts = [];
 
 function getPosts() {
-    if(start<100){
-        $.ajax({
-            url: `https://jsonplaceholder.typicode.com/posts?_start=${start}&_limit=9`,
-            type: "GET",
-            success: function (data, textStatus, jqXHR) {
-                posts=posts.concat(data);
-                printAllPosts(data);
-                start+=9;
-                canLoadMore=true;
-            },
-            error: function (jqXHR, textStatus, errorThrown) {},
-        });  
-    }
+  if (start < 100) {
+    $.ajax({
+      url: `https://jsonplaceholder.typicode.com/posts?_start=${start}&_limit=9`,
+      type: "GET",
+      success: function (data, textStatus, jqXHR) {
+        posts = posts.concat(data);
+        printAllPosts(data);
+        start += 9;
+        canLoadMore = true;
+      },
+      error: function (jqXHR, textStatus, errorThrown) {},
+    });
+  }
 }
 
 function printAllPosts(posts) {
-    posts.forEach((post) => {
-        printPost(post);
-    });
+  posts.forEach((post) => {
+    printPost(post);
+  });
 }
 
-function detectScrollBottom(){
-    $('.posts').scroll(function() {
-        let lastPost=$('.post:last-child');
-        if($(lastPost).position().top-$('.posts').height()+$(lastPost).height()<=0 && canLoadMore){
-            getPosts();
-            canLoadMore=false;
-        }
-     });
+function detectScrollBottom() {
+  $(".posts").scroll(function () {
+    detectBottom()
+  });
 }
-
+function detectBottom() {
+    console.log('aaaa');
+  let lastPost = $(".post:last-child");
+  if (
+    $(lastPost).position().top - $(".posts").height() + $(lastPost).height() <=
+      0 &&
+    canLoadMore
+  ) {
+    getPosts();
+    canLoadMore = false;
+  }
+}
 
 function getUser(id) {
-    $.ajax({
-        url: `https://jsonplaceholder.typicode.com/users/${id}`,
-        type: "GET",
-        success: function (data, textStatus) {
-            printUser(data);
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            // error callback
-        },
-    });
+  $.ajax({
+    url: `https://jsonplaceholder.typicode.com/users/${id}`,
+    type: "GET",
+    success: function (data, textStatus) {
+      printUser(data);
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      // error callback
+    },
+  });
 }
 
 function getComments(id) {
-    $.ajax({
-        url: `https://jsonplaceholder.typicode.com/posts/${id}/comments`,
-        type: "GET",
-        success: function (data, textStatus) {
-            console.log(data)
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-          // error callback
-        },
-      });
+  $.ajax({
+    url: `https://jsonplaceholder.typicode.com/posts/${id}/comments`,
+    type: "GET",
+    success: function (data, textStatus) {
+      console.log(data);
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      // error callback
+    },
+  });
 }
 
-function editPost(id, title, body){
-      $.ajax({
-        url: "https://jsonplaceholder.typicode.com/posts/"+id,
-        type: "PATCH",
-        timeout: 0,
-        headers: {
-            "Content-Type": "application/json",
-          },
-        data: JSON.stringify({"title":title,"body":body}),
-        success: function (data, textStatus, jqXHR) {
-            posts[id-1].title=title;
-            posts[id-1].body=body;
-            changePost(id, title, body);
-        },
-        error: function (jqXHR, textStatus, errorThrown) {},
-    }); 
+function editPost(id, title, body) {
+  $.ajax({
+    url: "https://jsonplaceholder.typicode.com/posts/" + id,
+    type: "PATCH",
+    timeout: 0,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: JSON.stringify({ title: title, body: body }),
+    success: function (data, textStatus, jqXHR) {
+      posts[id - 1].title = title;
+      posts[id - 1].body = body;
+      changePost(id, title, body);
+    },
+    error: function (jqXHR, textStatus, errorThrown) {},
+  });
 }
 
 function printPost(post) {
@@ -94,31 +101,40 @@ function printPost(post) {
 }
 
 function printUser(user) {
-    $(".modal--user").text(user.name);
-    $('.modal--username').text(user.username)
-    $('.modal--mail').text(user.email)
+  $(".modal--user").text(user.name);
+  $(".modal--username").text(user.username);
+  $(".modal--mail").text(user.email);
 }
 
-function deletePost(id){
-    let $post=$(`#${id}`);
-    $post.addClass('disaled');
-    $.ajax({
-        url: `https://jsonplaceholder.typicode.com/posts/${id}`,
-        type: "DELETE",
-        success: function (data, textStatus) {
-            $($post).fadeOut();
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-          // error callback
-          $post.removeClass('disaled');
-        },
-      });
+function deletePost(id) {
+  let $post = $(`#${id}`);
+  $post.addClass("disaled");
+  $.ajax({
+    url: `https://jsonplaceholder.typicode.com/posts/${id}`,
+    type: "DELETE",
+    success: function (data, textStatus) {
+      $($post).fadeOut(500, function(){
+        setTimeout(detectBottom, 10)})
+      
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      // error callback
+      $post.removeClass("disaled");
+    },
+  });
 }
 
-function changePost(id, title, body){
-    console.log(title);
-    $(`#${id} .post--content--title`).text(title);
-    $(`#${id} .post--content--body`).text(body);
+function changePost(id, title, body) {
+  console.log(title);
+  $(`#${id} .post--content--title`).text(title);
+  $(`#${id} .post--content--body`).text(body);
+}
+
+export {
+  getPosts,
+  getUser,
+  getComments,
+  deletePost,
+  editPost,
+  detectScrollBottom,
 };
-
-export { getPosts, getUser, getComments, deletePost,editPost, detectScrollBottom};
